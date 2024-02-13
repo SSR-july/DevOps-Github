ab51e4357881d6f39d5f1a56982f07bb52998d69
ssrjuly917 <158556224+ssrjuly917@users.noreply.github.com>
2024-02-13 16:09:20 +0530

    main.tf

--- abc.sh
+++ abc.sh
@@ -1,7 +1,9 @@
-Hi bay
-<<<<<<< HEAD
-can
+# Day-1/PROJECT-ec2-instance-creation/main.tf
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
 
-=======
-Don
->>>>>>> merge
+resource "aws_instance" "example" {
+    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+}
